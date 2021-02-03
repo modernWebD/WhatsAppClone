91eb73acf478df6451750e4272486b9adaf61e28
Rishabh_Sapkal <rishabhsapkal007@gmail.com>
2021-02-03 22:28:16 +0530

    Whatsapp Clone Master Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RoomDataService } from './Service/room-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Whatsap';
+
+  userSignedIn: boolean = false
+
+  constructor(private roomData: RoomDataService) {
+
+    if (this.roomData.user$) {
+
+      this.roomData.user$.subscribe(data => {
+
+        this.userSignedIn = true
+      })
+    }
+
+  }
 }
